bc6238d28041ec69bdd82f3929c35a9eea9e0aae
Virag Vora <42297255+viragvora@users.noreply.github.com>
2022-04-11 17:48:12 -0400

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -38,15 +38,15 @@ def test_model_class():
     assert opt.size(-1) == 8
     assert isinstance(model, torch.nn.Module)
 
-    train_loader, val_loader = mgr.train.data.get_data(mgr.configration.load_configurations()['data_path'])
+    train_loader, val_loader = mgr.train.data.get_data(mgr.configration.load.load_configurations()['data_path'])
     assert len(train_loader.shape) == 4
     assert len(val_loader.shape) == 4
-    assert train_loader.batch_size == mgr.configration.load_configurations()['train_batch_size']
-    assert val_loader.batch_size == mgr.configration.load_configurations()['valid_batch_size']
+    assert train_loader.batch_size == mgr.configration.load.load_configurations()['train_batch_size']
+    assert val_loader.batch_size == mgr.configration.load.load_configurations()['valid_batch_size']
     assert train_loader.shape[1:] == val_loader.shape[1:]
     assert train_loader.shape[1:] == (1, 128, 94)
-    assert train_loader.shape[0] == mgr.configration.load_configurations()['train_size']
-    assert val_loader.shape[0] == mgr.configration.load_configurations()['valid_size']
+    assert train_loader.shape[0] == mgr.configration.load.load_configurations()['train_size']
+    assert val_loader.shape[0] == mgr.configration.load.load_configurations()['valid_size']
 
 
     
