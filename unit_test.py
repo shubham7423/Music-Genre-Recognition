43e6debe1b19db622ec0bc01da730846a47a3611
Shubham Patel <41908939+shubham7423@users.noreply.github.com>
2022-04-12 14:55:23 -0400

    test case

--- unit_test.py
+++ unit_test.py
@@ -11,6 +11,7 @@ import mgr
 import mgr.train
 import torch
 import os
+import numpy as np
 
 def test_model_class():
     """Test the model class
@@ -21,6 +22,7 @@ def test_model_class():
     device = mgr.configuration.load_configurations()['device']
     random_inp = torch.rand(1, 1, 128, 94).to(device)
     
+    # test that the model outputs for 8 labels
     model = resnet.getModel()
     opt = model(random_inp)
     assert isinstance(opt, torch.Tensor)
@@ -53,23 +55,23 @@ def test_model_class():
 
     #check if the features and labels file exists
         #if you are not using all the models, comment out the model that you do not intend to use
-    assert os.path.exists('mgr/'+mgr.configuration.load.load_configurations()['cnn']['train']['features_path'])
-    assert os.path.exists('mgr/'+mgr.configuration.load.load_configurations()['cnn']['train']['labels_path'])
-    assert os.path.exists('mgr/'+mgr.configuration.load.load_configurations()['lstm']['train']['features_path'])
-    assert os.path.exists('mgr/'+mgr.configuration.load.load_configurations()['lstm']['train']['labels_path'])
-    assert os.path.exists('mgr/'+mgr.configuration.load.load_configurations()['transformer']['train']['features_path'])
-    assert os.path.exists('mgr/'+mgr.configuration.load.load_configurations()['transformer']['train']['labels_path'])
+    assert os.path.exists(mgr.configuration.load.load_configurations()['cnn']['train']['features_path'])
+    assert os.path.exists(mgr.configuration.load.load_configurations()['cnn']['train']['labels_path'])
+    assert os.path.exists(mgr.configuration.load.load_configurations()['lstm']['train']['features_path'])
+    assert os.path.exists(mgr.configuration.load.load_configurations()['lstm']['train']['labels_path'])
+    assert os.path.exists(mgr.configuration.load.load_configurations()['transformer']['train']['features_path'])
+    assert os.path.exists(mgr.configuration.load.load_configurations()['transformer']['train']['labels_path'])
 
     #if file exists, check if they are loaded properly, as the file is common, we can use any model's path
-    features, labels = mgr.train.data.load_data('mgr/'+mgr.configuration.load.load_configurations()['cnn']['train']['features_path'], 'mgr/' + mgr.configuration.load.load_configurations()['cnn']['train']['labels_path']);
+    features, labels = mgr.train.data.load_data(mgr.configuration.load.load_configurations()['cnn']['train']['features_path'], mgr.configuration.load.load_configurations()['cnn']['train']['labels_path']);
     assert len(features) != 0
     assert len(labels) != 0
     assert len(features) == len(labels)
     assert list(features.shape[1:]) == [1, 128, 94]
 
     #check if the preprocessing part directory exists
-    assert os.path.exists('mgr/' + mgr.configuration.load.load_configurations()['preprocessing']['train'])
-    assert os.path.exists('mgr/' + mgr.configuration.load.load_configurations()['preprocessing']['test'])
+    assert os.path.exists(mgr.configuration.load.load_configurations()['preprocessing']['train'])
+    assert os.path.exists(mgr.configuration.load.load_configurations()['preprocessing']['test'])
     
     #further validations of train_loader and val_loader variables using features and labels dataset
     train_loader, val_loader = mgr.train.data.get_data(features, labels);
@@ -85,9 +87,9 @@ def test_model_class():
     assert val_loader.batch_size == mgr.configuration.load.load_configurations()['transformer']['train']['valid_BS']
     
     #check if the folder exists where we save the models
-    assert os.path.exists('mgr/' + mgr.configuration.load.load_configurations()['cnn']['train']['save_model_at'])
-    assert os.path.exists('mgr/' + mgr.configuration.load.load_configurations()['lstm']['train']['save_model_at'])
-    assert os.path.exists('mgr/' + mgr.configuration.load.load_configurations()['transformer']['train']['save_model_at'])
+    assert os.path.exists(mgr.configuration.load.load_configurations()['cnn']['train']['save_model_at'])
+    assert os.path.exists(mgr.configuration.load.load_configurations()['lstm']['train']['save_model_at'])
+    assert os.path.exists(mgr.configuration.load.load_configurations()['transformer']['train']['save_model_at'])
     
     del(model)
     del(opt)
